14ec30a2bf3bcf3a96245a49fee2b35143d401ac
GmMamunH <gmmamun3004@gmail.com>
2025-03-02 06:13:49 +0600

    add(app): add route not found and error handling

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+// get html file
 app.get("/", (req, res) => {
   res.send("Welcome to the My Daily Task API");
 });
 
+
+
+//  route not found
+app.use((req, res, next)=>{
+    res.status(404).json({message: "Page not found"})
+})
+
+// server error response
+app.use((err, req, res, next) => {
+    res.status(500).json({message: "Something went wrong"})
+});
 module.exports = app;
