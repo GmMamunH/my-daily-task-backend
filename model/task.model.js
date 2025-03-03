569fe2953b59e85c0bc9f170e56d3e7ed89e9cb4
GmMamunH <gmmamun3004@gmail.com>
2025-03-03 15:10:53 +0600

    add(model): add completed item

--- model/task.model.js
+++ model/task.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const tasksSchema = mongoose.Schema({
   task: { type: "string", required: true },
+  completed: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
 });
 
